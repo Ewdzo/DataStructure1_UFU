8b0f76d4bf02ca52703d4bf3979484fda841038a
Enzo Weder <100097842+Ewdzo@users.noreply.github.com>
2023-03-03 07:59:34 -0300

    ✨ feature: add LISTA1_EX5 resolution

---
+++ LISTA1_EX5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(){
+    float price;
+    int aux;
+
+    printf("How many chickens currently? ");
+    scanf("%d", &aux);
+
+    price = (aux * 11.5);
+    printf("Final Cost: %.2f", %price);
+}
