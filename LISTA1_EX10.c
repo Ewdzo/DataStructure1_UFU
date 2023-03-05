e3ba5aae3333ef01105e108eb43f3febda271d46
Enzo Weder <enzowederma@gmail.com>
2023-03-05 19:13:28 -0300

    ✨ feat: add LISTA1_EX10 resolution

--- LISTA1_EX10.c
+++ LISTA1_EX10.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 void main(){
-    int code[5], vehicles[5], accidents[5], moreAccidents, lessAccidents;
-    float averageVehicles, averageAccidents;
+    int code[5], vehicles[5], accidents[5], moreAccidents, lessAccidents, moreCity = 0, lessCity = 0, smallCities = 0;
+    float averageVehicles = 0, averageAccidents = 0;
 
     for(int i = 0; i < 5; i++){
-        printf("Code of City: ");
+        printf("\nCode of City: ");
         scanf("%d", &code[i]);
 
         printf("Number of Vehicles: ");
@@ -15,9 +15,30 @@ void main(){
         scanf("%d", &accidents[i]);
 
         if(i == 0){
-            moreAccidents = accidents;
-            lessAccidents = accidents;
+            moreAccidents = accidents[i];
+            lessAccidents = accidents[i];
         }
+        else {
+            if( moreAccidents <= accidents[i] ) {
+                moreAccidents = accidents[i]; 
+                moreCity = code[i];
+            }
+            if( lessAccidents >= accidents[i] ) {
+                lessAccidents = accidents[i]; 
+                lessCity = code[i];
+            }
+        }
+
+        if(vehicles[i] < 2000){
+            smallCities++; 
+            averageAccidents += accidents[i];
+        }
+
+        averageVehicles += (vehicles[i]/5);
     }
+
+    printf("\nMost Accidents: %d in city code %d.\nLess Accidents; %d in city code %d.", moreAccidents, moreCity, lessAccidents, lessCity);
+    printf("\nAverage Vehicles: %f", averageVehicles);
+    printf("\nAverage Accidents in Small Cities: %f", averageAccidents);
 
 }
