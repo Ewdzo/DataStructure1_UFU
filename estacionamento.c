c010ef4c3fb1c0bc7ecc9b1bd265e283cc4302e0
Enzo Weder <enzowederma@gmail.com>
2023-04-20 19:43:26 -0300

    🎈 perf: remove struct car

--- estacionamento.c
+++ estacionamento.c
@@ -3,79 +3,70 @@
 #define N 10
 
 typedef struct{
-    char licensePlate[8];
-} car;
-
-typedef struct{
     int top;
-    car cars[N];
+    char licensePlate[N][8];
 } stack;
 
-void push(car x, stack *stack){
+void push(char *x, stack *stack){
     if ((*stack).top == (N-1)) printf("%s", "stack overflow");
-    else (*stack).cars[(++(*stack).top)] = x;
+    else *(*stack).licensePlate[(++(*stack).top)] = *x;
 }
 
-car pop(stack *stack){
+char *pop(stack *stack){
     if ((*stack).top == -1){
         printf("%s", "stack underflow");
-
-        car aux;
-        return(aux);
+        return '0';
     }
-    else return((*stack).cars[(*stack).top--]);
+    else return((*stack).licensePlate[(*stack).top--]);
 };
 
 void main(){
     stack parked, waiting;
     char input[10];
+    char aux[8];
 
     while(1){
-        setbuf(0, stdin);
         printf("\nInsert Char and Plate (Ex: C AAA-0000): ");
-        fgets(input, 11, stdin);
+        gets(input);
+        setbuf(NULL, stdin);
 
         if(input[0] == 'C') {
-            car aux;
-
-            for(int i = 0; i<9; i++) { aux.licensePlate[i] = input[i+2]; }
+            for(int i = 0; i<9; i++) { aux[i] = input[i+2]; }
 
             if(parked.top != (N-1)) {
                 push(aux, &parked);
-                printf("\n%s - Parked ", aux.licensePlate);
+                printf("\n%s - Parked ", aux);
             }
             else {
-                printf("\n%s - Waiting ", aux.licensePlate);
+                printf("\n%s - Waiting ", aux);
                 push(aux, &waiting);
             };
         }
-        // else if(input[0] == 'P'){
-        //     car aux;
-        //     int position = -1;
+        else if(input[0] == 'P') {
+            printf("hi"); 
+            int position = -1;
 
-        //     for(int i = 0; i<9; i++) { aux.licensePlate[i] = input[i+2]; }
+            for(int i = 0; i<9; i++) {
+                aux[i] = input[i+2]; 
+            }
 
-        //     for(int i = 0; i <= parked.top; i++) {
-        //         if(aux.licensePlate == parked.cars[i].licensePlate) position = i;
-        //     };
+            for(int i = 0; i <= parked.top; i++) { 
+                if(aux == parked.licensePlate[i]) position = i; 
+            };
 
-        //     if(position != -1){
-        //         for(int i = parked.top; i>position; i--) {
-        //             printf("\n%s - Waiting ", parked.cars[i].licensePlate);
-        //             push(pop(&parked), &waiting);
-        //         }
-        //         pop(&parked);
+            if(position != -1){
+                for(int i = parked.top; i>=position; i--) {
+                    printf("\n%s - Waiting ", parked.licensePlate[i]);
+                    push(pop(&parked), &waiting);
+                }
+                pop(&parked);
 
-        //         for(int i = parked.top; i != (parked.top- 1); i++) {
-        //             printf("\n%s - Parked ", waiting.cars[i].licensePlate);
-        //             push(pop(&waiting), &parked);
-        //         }
-        //     }
-        //     else printf("\n%s - Not Found", aux.licensePlate);
-        // }
+                for(int i = parked.top; i != (parked.top- 1); i++) {
+                    printf("\n%s - Parked ", waiting.licensePlate[i]);
+                    push(pop(&waiting), &parked);
+                }
+            }
+            else printf("\n%s - Not Found", aux);
+        }
     }
-
-
-    
-
 }
