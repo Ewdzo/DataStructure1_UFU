a290d14b0ecbabae83f957eebdeb4bf3ecae42ac
Enzo Weder <enzowederma@gmail.com>
2023-05-30 22:08:09 -0300

    ✨ feat: add option menu

--- TRAB_FINAL/TRAB_FINAL.c
+++ TRAB_FINAL/TRAB_FINAL.c
@@ -33,3 +33,14 @@ typedef struct {
     struct node *prox;
     struct node *ant;
 } produto;
+
+void main(){
+    int option = 0;
+    cliente *inicio_cliente = NULL;
+    produto *inicio_produto = NULL;
+
+    while(option != 19){
+        printf("\n1 - Insercao de Cliente\n2 - Alteracao de Cliente\n3 - Consulta de Cliente\n4 - Delecao de Cliente\n\n5 - Insercao de Produto\n6 - Alteracao de Produto\n7 - Consulta de Produto\n8 - Delecao de Produto\n\n9 - Insercao de Venda\n10 - Alteracao de Venda\n11 - Consulta de Venda\n12 - Delecao de Venda\n\n14 - Listar Clientes\n15 - Listar Produtos\n16 - Listar Vendas\n17 - Checar Clientes por Gasto\n18 - Checar Estoque Baixo\n19 - Sair\n\nOpcao: ");
+        scanf("%d", &option);
+    }
+}
