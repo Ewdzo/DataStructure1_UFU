87fee9d495e748a01746ea0c42c285d7e43d61f0
Enzo Weder <enzowederma@gmail.com>
2023-05-30 23:02:05 -0300

    🐞 fix: change typing and clear buffer

--- TRAB_FINAL/TRAB_FINAL.c
+++ TRAB_FINAL/TRAB_FINAL.c
@@ -53,7 +53,7 @@ void insereCliente(cliente *clienteNovo, long long int cpf, char nome[50], long 
     cliente *aux;
 
     clienteNovo->cpf = cpf;
-    clienteNovo->nome = nome;
+    strcpy(clienteNovo->nome, nome);
     clienteNovo->telefone = telefone;
     clienteNovo->endereco = endereco_cliente;
     clienteNovo->data_nasc = data_nasc;
@@ -68,39 +68,49 @@ void main(){
 
     while(option != 19){
         printf("\n1 - Insercao de Cliente\n2 - Alteracao de Cliente\n3 - Consulta de Cliente\n4 - Delecao de Cliente\n\n5 - Insercao de Produto\n6 - Alteracao de Produto\n7 - Consulta de Produto\n8 - Delecao de Produto\n\n9 - Insercao de Venda\n10 - Alteracao de Venda\n11 - Consulta de Venda\n12 - Delecao de Venda\n\n14 - Listar Clientes\n15 - Listar Produtos\n16 - Listar Vendas\n17 - Checar Clientes por Gasto\n18 - Checar Estoque Baixo\n19 - Sair\n\nOpcao: ");
+        
         scanf("%d", &option);
+        setbuf(stdin, NULL);
 
         if(option == 1){
-            cliente aux_cliente;
+            cliente *aux_cliente;
             long long int cpf_aux, telefone_aux;
             char nome_aux[50];
             endereco endereco_cliente_aux;
             data data_nasc_aux;
 
             printf("\nInsira o CPF: ");
+            setbuf(stdin, NULL);
             scanf("%lld", &cpf_aux);
 
             printf("\nInsira o Nome: ");
+            setbuf(stdin, NULL);
             gets(nome_aux);
 
             printf("\nInsira o Telefone: ");
+            setbuf(stdin, NULL);
             scanf("%lld", &telefone_aux);
 
             printf("\nInsira a Rua do Endereco: ");
+            setbuf(stdin, NULL);
             gets(endereco_cliente_aux.rua);
 
             printf("\nInsira o Numero do Endereco: ");
+            setbuf(stdin, NULL);
             scanf("%d", &(endereco_cliente_aux.numero));
 
             printf("\nInsira a Cidade do Endereco: ");
+            setbuf(stdin, NULL);
             gets(endereco_cliente_aux.cidade);
 
             printf("\nInsira o Estado do Endereco: ");
+            setbuf(stdin, NULL);
             gets(endereco_cliente_aux.estado);
 
             printf("\nInsira o Dia do Nascimento (DD/MM/AAAA):");
+            setbuf(stdin, NULL);
             scanf("%d/%d/%d", &(data_nasc_aux.dia), &(data_nasc_aux.mes), &(data_nasc_aux.ano));
-
+            setbuf(stdin, NULL);
 
             aux_cliente = (cliente *)malloc(sizeof(cliente));
  
