155323cb3e0aac6dfdb271e29cdbe31f45058f92
Enzo Weder <enzowederma@gmail.com>
2023-05-30 21:59:33 -0300

    ✨ feat: add structs into TRAB_FINAL
    Add data, endereco, cliente, produto;

--- TRAB_FINAL/TRAB_FINAL.c
+++ TRAB_FINAL/TRAB_FINAL.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
-struct node {
-    int data;
-    struct node *next;
-    struct node *prev;
-};
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+} data;
+
+typedef struct {
+    char rua[50];
+    int numero;
+    char cidade[50];
+    char estado[50];
+} endereco;
+
+typedef struct node{
+    long long int cpf;
+    char nome[50];
+    long long int telefone;
+    endereco endereco;
+    data data_nasc;
+
+    struct node *prox;
+    struct node *ant;
+} cliente;
+
+typedef struct {
+    int codigo;
+    char descricao[100];
+    int estoque;
+    int preco_unitario;
+
+    struct node *prox;
+    struct node *ant;
+} produto;
